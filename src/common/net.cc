0d95b60a55d9acec6e3f501751b7ac93b33ca2e1
Rahul Palamuttam <rahulpalamut@gmail.com>
2017-07-17 14:42:52 -0700

    hostname

--- src/common/net.cc
+++ src/common/net.cc
@@ -3,12 +3,15 @@
 #include <arpa/inet.h>
 
 #include <sstream>
-
+#include <iostream>
 #include "common.h"
 
 int parse_ip_addr_port(const char *ip_addr_port, char *ip_addr, int *port) {
   char port_str[6];
-  int parsed = sscanf(ip_addr_port, "%15[0-9.]:%5[0-9]", ip_addr, port_str);
+  int parsed = sscanf(ip_addr_port, "%[^:]:%5[0-9]", ip_addr, port_str);
+  std::cout << ip_addr << std::endl;
+  std::cout << port_str << std::endl;
+  std::cout << ip_addr_port << std::endl;
   if (parsed != 2) {
     return -1;
   }
